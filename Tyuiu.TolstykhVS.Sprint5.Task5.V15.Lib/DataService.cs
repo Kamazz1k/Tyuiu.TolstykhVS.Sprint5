8192e1139ff7cd15a42eb9cb4cb0409a6efbfefc
Kamazz1k <144707479+Kamazz1k@users.noreply.github.com>
2023-11-26 20:33:42 +0500

    Task5

---
+++ Tyuiu.TolstykhVS.Sprint5.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+
+namespace Tyuiu.TolstykhVS.Sprint5.Task5.V15.Lib
+{
+    public class DataService : ISprint5Task5V10
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+
+                {
+                    double num = Convert.ToDouble(line);
+                    if (num % 5 == 0)
+                    {
+                        double min = 0;
+                        if (num > min)
+                        {
+                            min = num;
+                        }
+                        res = min;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.TolstykhVS.Sprint5.Task5.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TolstykhVS.Sprint5.Task5.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TolstykhVS.Sprint5.Task5.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1fb5e0b3-d1ef-4c63-a6ba-0cbe7e8a65e8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
